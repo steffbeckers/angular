aed0a1cf610498246532a154e9d3b8297246d085
Steff Beckers <steff@steffbeckers.eu>
2020-06-26 00:45:41 +0200

    ng generate feature admin/store/Admin -m admin/admin.module.ts --group

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import * as fromAdmin from './store/reducers/admin.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AdminEffects } from './store/effects/admin.effects';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, StoreModule.forFeature(fromAdmin.adminFeatureKey, fromAdmin.reducer), EffectsModule.forFeature([AdminEffects])],
 })
 export class AdminModule {}
---
+++ src/app/admin/store/reducers/admin.reducer.ts
@@ -0,0 +1,21 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import * as AdminActions from '../actions/admin.actions';
+
+export const adminFeatureKey = 'admin';
+
+export interface State {
+
+}
+
+export const initialState: State = {
+
+};
+
+
+export const reducer = createReducer(
+  initialState,
+
+  on(AdminActions.loadAdmins, state => state),
+
+);
+
