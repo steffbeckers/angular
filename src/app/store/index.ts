70525ac1d1b4d14564ac1e5f7ca1629c84831d24
Steff Beckers <steff@steffbeckers.eu>
2020-07-04 19:01:47 +0200

    Storing ngrx store in localstorage

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,20 +1,13 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 import { environment } from '../../environments/environment';
 
+export interface State {}
 
-export interface State {
+export const reducers: ActionReducerMap<State> = {};
 
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({ keys: ['auth'], rehydrate: true })(reducer);
 }
 
-export const reducers: ActionReducerMap<State> = {
-
-};
-
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [localStorageSyncReducer] : [localStorageSyncReducer];
