4b6eb9565d6077632d8eb1137e2a79b830e6bac5
Steff Beckers <steff@steffbeckers.eu>
2020-07-05 15:12:48 +0200

    App online/offline effect

--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
 // RxJS
-import { interval } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
+import { interval, of, merge, fromEvent } from 'rxjs';
+import { mapTo, map } from 'rxjs/operators';
 
 // NgRx
 import { Actions, createEffect } from '@ngrx/effects';
@@ -14,5 +14,14 @@ export class AppEffects {
 
   every5Seconds$ = createEffect((): any => {
     return interval(5000).pipe(mapTo(CounterActions.increment()));
+  });
+
+  online$ = createEffect((): any => {
+    return merge(of(navigator.onLine), fromEvent(window, 'online').pipe(mapTo(true)), fromEvent(window, 'offline').pipe(mapTo(false))).pipe(
+      map((online) => {
+        // TODO: Add app action and reducer to store online state
+        return online ? CounterActions.increment() : CounterActions.decrement();
+      })
+    );
   });
 }
