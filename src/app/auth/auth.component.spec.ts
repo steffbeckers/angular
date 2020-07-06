aa1e5f14cba85341648b8389245a70f9dedb819d
Steff Beckers <steff@steffbeckers.eu>
2020-07-06 23:07:08 +0200

    auth component spec tried to add mock store

--- src/app/auth/auth.component.spec.ts
+++ src/app/auth/auth.component.spec.ts
@@ -1,20 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+// NgRx
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+
 import { AuthComponent } from './auth.component';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
+  let mockStore: MockStore;
   let fixture: ComponentFixture<AuthComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AuthComponent],
+      providers: [provideMockStore()],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AuthComponent);
     component = fixture.componentInstance;
+    mockStore = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
